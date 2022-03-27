c25fe2128c76df61a2bbc58db12fdac36051baf2
user <user@Workshop-02>
2022-03-27 16:55:35 +0530

    Pushing the code first time

---
+++ Project 2/src/Pkg1/Class1.java
@@ -0,0 +1,16 @@
+package Pkg1;
+
+public class Class1 
+{
+public void Method1()
+{
+	System.out.println("We need to open the browser for surfing");
+	
+}
+public static void main(String[] args) 
+{
+	Class1 obj=new Class1();
+	obj.Method1();
+}
+}
+
